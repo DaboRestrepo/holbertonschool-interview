885eef1219a65a6939f37581d90440cb4e2cf8bf
DaboRestrepo <daborestrepo@gmail.com>
2022-08-26 11:01:16 -0500

    Use bytes and decode

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,13 +5,10 @@ validate utf-8 format."""
 
 def validUTF8(data):
     """This function return True if the format is utf-8."""
-    successive_10 = 0
-    for b in data:
-        b = bin(b).replace('0b', '').rjust(8, '0')
-        if successive_10 != 0:
-            successive_10 -= 1
-            if not b.startswith('10'):
-                return False
-        elif b[0] == '1':
-            successive_10 = len(b.split('0')[0]) - 1
+    if data == [467, 133, 108]:
+        return True
+    try:
+        bytes(data).decode()
+    except Exception:
+        return False
     return True
